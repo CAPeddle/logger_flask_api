1d585d12c725c87574f72bc3beb7c15996c5b9c1
CAPeddle <chrisapeddle@gmail.com>
2021-06-18 21:26:48 +0100

    add static html

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Python Flask-Restful api to log data points
 
 need to convert the rest api to the format of the mega tut so that it's easier to use the advice there
+
+use this to add static html - javascript to the project
+https://realpython.com/flask-connexion-rest-api/
+
+to get to the config
+return app.config['SECRET_KEY']
--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, render_template
 from flask_restful import Api, Resource, reqparse, fields, marshal
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__, static_url_path="")
 api = Api(app)
-
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 tasks = [
     {
@@ -90,9 +93,19 @@ class TaskAPI(Resource):
         tasks.remove(task[0])
         return {'result': True}
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)  
+        
 @app.route('/')
 def index():
-    return app.config['SECRET_KEY']
+    return render_template('home.html')
 
 api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
 api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
